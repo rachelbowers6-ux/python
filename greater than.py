b000062f8ccccb48bc0247249e40add9b7ab89eb
rachelbowers6-ux <rachelbowers6@gmail.com>
2026-03-07 21:00:11 -0500

    Add files via upload

---
+++ greater than.py
@@ -0,0 +1,16 @@
+def is_greater(num1, num2):
+    return num1 > num2
+
+a = 2
+b = 3
+result_1 = is_greater(a, b)
+
+print_statement_1 = "The statement " + str(a) + " is greater than " + str(b) + " is " + str(result_1)
+print(print_statement_1)
+
+a = 10
+b = 6
+result_2 = is_greater(a, b)
+
+print_statement_2 = "The statement " + str(a) + " is greater than " + str(b) + " is " + str(result_2)
+print(print_statement_2)
